7df8bcc9e21fe1f3a1b3bbcf2f0361ed5412dcbb
hoslo <ys952572463@gmail.com>
2024-04-02 09:50:26 +0800

    feat(services/github): support list recursive

--- core/src/services/github/lister.rs
+++ core/src/services/github/lister.rs
@@ -24,27 +24,34 @@ use crate::*;
 
 pub struct GithubLister {
     core: Arc<GithubCore>,
+
     path: String,
+    recursive: bool,
 }
 
 impl GithubLister {
-    pub fn new(core: Arc<GithubCore>, path: &str) -> Self {
+    pub fn new(core: Arc<GithubCore>, path: &str, recursive: bool) -> Self {
         Self {
             core,
 
             path: path.to_string(),
+            recursive,
         }
     }
 }
 
 impl oio::PageList for GithubLister {
     async fn next_page(&self, ctx: &mut oio::PageContext) -> Result<()> {
-        let entries = self.core.list(&self.path).await?;
+        let resp = self.core.list(&self.path).await?;
 
-        for entry in entries {
+        // Record whether there is a dir in the list so that we need recursive later.
+        let mut has_dir = false;
+
+        for entry in resp.entries {
             let path = build_rel_path(&self.core.root, &entry.path);
             let entry = if entry.type_field == "dir" {
                 let path = format!("{}/", path);
+                has_dir = true;
                 Entry::new(&path, Metadata::new(EntryMode::DIR))
             } else {
                 if path.ends_with(".gitkeep") {
@@ -55,8 +62,42 @@ impl oio::PageList for GithubLister {
                     .with_etag(entry.sha);
                 Entry::new(&path, m)
             };
-
             ctx.entries.push_back(entry);
+        }
+
+        // if recursive is true and there is a dir in the list, we need to list it recursively.
+        if self.recursive && has_dir {
+            let tree = self.core.list_with_recursive(&resp.git_url).await?;
+            ctx.entries.clear();
+            for t in tree {
+                let path = if self.path == "/" {
+                    t.path
+                } else {
+                    format!("{}/{}", self.path, t.path)
+                };
+                let entry = if t.type_field == "tree" {
+                    let path = format!("{}/", path);
+                    Entry::new(&path, Metadata::new(EntryMode::DIR))
+                } else {
+                    if path.ends_with(".gitkeep") {
+                        continue;
+                    }
+                    let mut m = Metadata::new(EntryMode::FILE).with_etag(t.sha);
+
+                    if let Some(size) = t.size {
+                        m = m.with_content_length(size);
+                    }
+                    Entry::new(&path, m)
+                };
+                ctx.entries.push_back(entry);
+            }
+        }
+
+        if !self.path.ends_with('/') {
+            ctx.entries.push_back(Entry::new(
+                &format!("{}/", self.path),
+                Metadata::new(EntryMode::DIR),
+            ));
         }
 
         ctx.done = true;
